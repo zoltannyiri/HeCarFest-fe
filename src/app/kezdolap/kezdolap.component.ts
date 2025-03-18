cab00a88312975aa34dc5056b6ea7a10ae3c5998
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-03-18 17:16:13 +0100

    try

--- src/app/kezdolap/kezdolap.component.ts
+++ src/app/kezdolap/kezdolap.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-kezdolap',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './kezdolap.component.html',
   styleUrl: './kezdolap.component.scss'
 })
